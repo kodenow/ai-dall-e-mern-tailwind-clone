369fcc5e5c3fa2c3577fc0f19dacc4ed746868af
Kris <kodenow13@gmail.com>
2023-02-27 07:17:11 +0800

    RenderCards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { BLOCKED_PAGES } from 'next/dist/shared/lib/constants';
 import React, {useState, useEffect} from 'react';
 import {Loader, Card,FormField} from '../components';
 
 const RenderCards = ({data, title}) => {
-  if(data?.length > 0) return data.map((post) => <Card key={post._id} {...post}/>)
+  if (data?.length > 0) { return data.map((post) => <Card key={post._id} {...post}/>) }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  )
 }
 
 
@@ -30,7 +35,15 @@ const Home = () => {
               {searchText && (
                 <h2 className='font-medium text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328]'>{searchText}</span></h2>
               )}
-              <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'></div>
+              <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                {searchText ? (
+                  <RenderCards data={[]} title="No search results found" />
+                ) : (
+                  //if there are no search results, try to find if there are posts(data), if not, "no posts found"(title) will be displayed
+                  <RenderCards data={[]} title="No posts found" /> 
+
+                )}
+              </div>
             </>
           )}
         </div>
