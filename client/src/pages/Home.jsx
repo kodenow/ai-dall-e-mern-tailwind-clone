f8e07e94e339701a5860d86be815459a19d158af
Kris <kodenow13@gmail.com>
2023-03-09 09:45:11 +0800

    backend URL

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://dall-e-backend-67dc.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
